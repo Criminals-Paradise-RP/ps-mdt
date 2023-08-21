5cc14007de8b35b644bb8cf8315a5749eca10c96
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2023-08-20 17:58:35 -0700

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -1,7 +1,7 @@
 Config = Config or {}
 
 Config.UsingPsHousing = true
-Config.UsingDefaultQBApartments = true
+Config.UsingDefaultQBApartments = false
 Config.OnlyShowOnDuty = true
 Config.UseCQCMugshot = true
 
@@ -21,7 +21,7 @@ Config.QBManagementUse = false
 -- However, if you're using a different inventory system, please refer to the "Inventory Edit | Automatic Add Weapons with images" section in ps-mdt's README.
 Config.InventoryForWeaponsImages = "ps-inventory"
 
--- "LegacyFuel", "lj-fuel", "ps-fuel"
+-- "cdn-fuel", "lj-fuel", "ps-fuel"
 Config.Fuel = "cdn-fuel"
 
 -- Google Docs Link
